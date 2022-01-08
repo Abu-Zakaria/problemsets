55f9f86d95afb3ad547e85abfbd0036a2dcd8c2c
Jakaria Blaine <jakariablaine120@gmail.com>
2022-01-09 01:57:35 +0600

    More Solves

---
+++ 160/A.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define PRINT(stuff) std::cout << stuff << endl
+#define PRINT_NO_LINE(stuff) std::cout << stuff
+#define PRINT_VEC(stuff)                      \
+    for (size_t i = 0; i < stuff.size(); i++) \
+    {                                         \
+        PRINT(stuff.at(i));                   \
+    }
+#define PRINT_VEC_NO_LINE(stuff)              \
+    for (size_t i = 0; i < stuff.size(); i++) \
+    {                                         \
+        PRINT_NO_LINE(stuff.at(i));           \
+    }
+#define FOR(i, a) for (size_t(i) = 0; i < (a); ++(i))
+#define FXR(i, start, a) for (size_t(i) = (start); i < (a); ++(i))
+#define vecstr vector<string>
+#define vecint vector<int>
+#define isodd(x) x % 2 != 0
+#define iseven(x) x % 2 == 0
+#define all(x) (x).begin(), (x).end()
+int gcd(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    return gcd(b % a, a);
+}
+int nxt()
+{
+    int x;
+    cin >> x;
+    return x;
+}
+template <typename T>
+T nxt()
+{
+    T x;
+    cin >> x;
+    return x;
+}
+
+void solve()
+{
+    int n = nxt();
+    vecint coins;
+    FOR(i, n)
+    {
+        coins.push_back(nxt());
+    }
+    sort(all(coins), greater<int>());
+
+    int me = coins.at(0), bro = 0, ans = 1;
+
+    FXR(i, 1, n)
+    {
+        int a = coins.at(i);
+        if (bro + a >= me)
+        {
+            me += a;
+            ans++;
+        }
+        else
+        {
+            bro += a;
+        }
+    }
+    PRINT(ans);
+}
+
+int main()
+{
+#ifdef ONLINE_JUDGE
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#endif
+#ifndef ONLINE_JUDGE
+    ifstream cin("input.txt");
+
+    std::cin.rdbuf(cin.rdbuf());
+#endif
+    solve();
+}
